7c844f5f7149c7a53619d560dd23e17ebb080ab0
YonghoAhn <hrh1110@naver.com>
2019-08-14 10:25:17 +0900

    [Add] fragments for register, Material Theme.
    RegisterFragment, LoginFragment added.
    Custom Material Theme (Color, Shape, Typography)
    Add dependencies for navigation and material components.

---
+++ app/src/main/java/moe/misakachan/runway/Fragments/RegisterFragment.kt
@@ -0,0 +1,34 @@
+package moe.misakachan.runway.Fragments
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import moe.misakachan.runway.R
+import moe.misakachan.runway.ViewModels.RegisterViewModel
+
+class RegisterFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = RegisterFragment()
+    }
+
+    private lateinit var viewModel: RegisterViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.register_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(RegisterViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
