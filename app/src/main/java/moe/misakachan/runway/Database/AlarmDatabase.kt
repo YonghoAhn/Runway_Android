7c65cb912e2d9dcc2636a00260a49fa3617826d2
YonghoAhn <hrh1110@naver.com>
2019-08-17 08:18:41 +0900

    [Add] add Alarm Database class
    add abstract Database class using Room
    applied SingleTon pattern and synchronized for Multi-Tasking

---
+++ app/src/main/java/moe/misakachan/runway/Database/AlarmDatabase.kt
@@ -0,0 +1,29 @@
+package moe.misakachan.runway.Database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import moe.misakachan.runway.Models.Alarm
+import moe.misakachan.runway.Models.AlarmDAO
+
+@Database(entities = [Alarm::class], version = 1)
+abstract class AlarmDatabase: RoomDatabase()
+{
+    abstract fun alarmDAO() : AlarmDAO
+
+    //for SingleTon
+    companion object{
+        private var Instance: AlarmDatabase? = null
+        fun getInstance(context: Context): AlarmDatabase{
+            if(Instance==null)
+            {
+                synchronized(AlarmDatabase::class)
+                {
+                    Instance = Room.databaseBuilder(context.applicationContext, AlarmDatabase::class.java, "alarm").fallbackToDestructiveMigration().build()
+                }
+            }
+            return this.Instance!!
+        }
+    }
+}
