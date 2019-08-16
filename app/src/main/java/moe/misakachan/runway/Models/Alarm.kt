a2bc10c862e702820f6c5682edfb46ea17d9515c
YonghoAhn <hrh1110@naver.com>
2019-08-17 08:09:49 +0900

    [Add] Editing AlarmModel
    add Room, LiveData, and ViewModel to build.gradle
    add Entity to AlarmModel

--- app/src/main/java/moe/misakachan/runway/Models/Alarm.kt
+++ app/src/main/java/moe/misakachan/runway/Models/Alarm.kt
@@ -1,3 +1,34 @@
 package moe.misakachan.runway.Models
 
-data class Alarm (var time : String, var color : String, var volume : Int, val stuff : ArrayList<String>)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//this is an Entity class.
+@Entity(tableName = "alarm")
+data class Alarm (
+    @PrimaryKey
+    var time : String,
+    @ColumnInfo(name = "color")
+    var color : String,
+    @ColumnInfo(name = "volume")
+    var volume : Int,
+    @ColumnInfo(name = "weekday")
+    var weekday : Array<Boolean>,
+    @ColumnInfo(name = "stuff")
+    val stuff : ArrayList<String>
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Alarm
+        if (!weekday.contentEquals(other.weekday)) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return weekday.contentHashCode()
+    }
+
+    constructor() : this("0000","FFFFFF",100, Array(7) {true}, arrayListOf<String>())
+}
