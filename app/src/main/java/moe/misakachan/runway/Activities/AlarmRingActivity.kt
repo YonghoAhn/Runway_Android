1d4638faf87a22792e87cefa0f7e2988ffd0dff6
YonghoAhn <hrh1110@naver.com>
2019-08-17 01:56:59 +0900

    [Add] UI work
    add alarm ring activity
    
    modified strings and dimens, navigation

---
+++ app/src/main/java/moe/misakachan/runway/Activities/AlarmRingActivity.kt
@@ -0,0 +1,13 @@
+package moe.misakachan.runway.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import moe.misakachan.runway.R
+
+class AlarmRingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm_ring)
+    }
+}
