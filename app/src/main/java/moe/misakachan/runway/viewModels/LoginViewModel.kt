6f96feb8441204212d2db6130e17e95cb5cc4302
YonghoAhn <hrh1110@naver.com>
2019-08-24 11:49:10 +0900

    [Add] Facebook Auth

--- app/src/main/java/moe/misakachan/runway/viewModels/LoginViewModel.kt
+++ app/src/main/java/moe/misakachan/runway/viewModels/LoginViewModel.kt
@@ -1,10 +1,17 @@
 package moe.misakachan.runway.viewModels
 
+import android.view.View
 import androidx.lifecycle.ViewModel;
+import com.facebook.login.LoginManager
 
 class LoginViewModel : ViewModel() {
 
     override fun onCleared() {
         super.onCleared()
+    }
+
+    fun onClick(view: View)
+    {
+        val loginManager = LoginManager.getInstance()
     }
 }
