e419d2e4063469214cec37e4e6cbf75f5f912338
YonghoAhn <hrh1110@naver.com>
2019-09-04 18:40:47 +0900

    [Add] DeviceManagement Fragment naviagtion

--- app/src/main/java/moe/misakachan/runway/fragments/DeviceManagementFragment.kt
+++ app/src/main/java/moe/misakachan/runway/fragments/DeviceManagementFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.device_management_fragment.*
 
 import moe.misakachan.runway.R
 import moe.misakachan.runway.viewModels.DeviceManagementViewModel
@@ -22,13 +25,23 @@ class DeviceManagementFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.device_management_fragment, container, false)
+        val view = inflater.inflate(R.layout.device_management_fragment, container, false)
+        view.findViewById<MaterialButton>(R.id.btnMirror).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_deviceManagementFragment_to_mirrorManageFragment)
+        }
+        view.findViewById<MaterialButton>(R.id.btnAlarm).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_deviceManagementFragment_to_alarmManageFragment)
+        }
+        view.findViewById<MaterialButton>(R.id.btndoorlock).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_deviceManagementFragment_to_doorManageFragment)
+        }
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(DeviceManagementViewModel::class.java)
-        // TODO: Use the ViewModel
+
     }
 
 }
