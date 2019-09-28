2f8544aaea728fe28bcc0411352e3688ef8640af
YonghoAhn <hrh1110@naver.com>
2019-09-29 07:10:19 +0900

    [Add] Alarm Ring activity

--- app/src/main/java/moe/misakachan/runway/activities/AlarmRingActivity.kt
+++ app/src/main/java/moe/misakachan/runway/activities/AlarmRingActivity.kt
@@ -2,6 +2,8 @@ package moe.misakachan.runway.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_alarm_ring.*
 import moe.misakachan.runway.R
 
 class AlarmRingActivity : AppCompatActivity() {
@@ -9,5 +11,9 @@ class AlarmRingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm_ring)
+        btnStopAlarm.setOnClickListener {
+            FirebaseDatabase.getInstance().getReference("/triggered").setValue(false)
+            finish()
+        }
     }
 }
--- app/src/main/java/moe/misakachan/runway/services/FCMService.kt
+++ app/src/main/java/moe/misakachan/runway/services/FCMService.kt
@@ -3,9 +3,11 @@ package moe.misakachan.runway.services
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import moe.misakachan.runway.activities.AlarmRingActivity
 
 class FCMService : FirebaseMessagingService() {
     override fun onNewToken(p0: String) {
@@ -15,6 +17,12 @@ class FCMService : FirebaseMessagingService() {
 
     override fun onMessageReceived(p0: RemoteMessage) {
         super.onMessageReceived(p0)
+        if(p0.notification!!.title!! == "Alarm Ring")
+        {
+            FirebaseDatabase.getInstance().getReference("/triggered").setValue(true)
+            startActivity(Intent(applicationContext,AlarmRingActivity::class.java))
+        }
+        Log.d("MisakaMOE", p0.notification!!.title!!)
     }
 
 }
